339e0e5996989493026db0427efbf668dc3e0569
claire-tsai <qs2536@gmail.com>
2018-05-18 17:00:04 +0800

    src

---
+++ src/Arudino/node2/node2.ino
@@ -0,0 +1,27 @@
+// Declare Constants
+#define smoke_pin 3		//Digital IO PIN3
+#define BT_RxD 10
+#define BT_TxD 11
+
+// Declare Serial port
+#include <SoftwareSerial.h>
+SoftwareSerial BT(BT_RxD,BT_TxD); 
+
+// Declare Variables
+int smoke_state=0;  /* Holds the last state of the switch */
+char smoke=0;
+
+void setup()   
+{
+  Serial.begin(9600);
+  BT.begin(19200);
+}
+void loop()  
+{
+  smoke_state = digitalRead(smoke_pin);
+  if(smoke_state=HIGH) smoke = 'l'; 
+  else if(smoke_state=LOW) smoke = 'h';
+  Serial.print(smoke);  
+  BT.write(smoke);
+}
+
